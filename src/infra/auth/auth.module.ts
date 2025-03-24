6f7b3ad07c6b478255dfbb0f7668f92fa289294a
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-03-24 16:25:30 -0300

    refactor: ajustando o guard do nest para que todas as rotas sejam privadas por padrão

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { EnvModule } from '../env/env.module';
 import { EnvService } from '../env/env.service';
+import { JwtAuthGuard } from './jwt-auth-guard';
 import { JwtStrategy } from './jwt-strategy';
-
 @Module({
   imports: [
     PassportModule,
@@ -26,6 +27,13 @@ import { JwtStrategy } from './jwt-strategy';
       },
     }),
   ],
-  providers: [JwtStrategy, EnvService],
+  providers: [
+    JwtStrategy,
+    EnvService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
